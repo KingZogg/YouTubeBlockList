e29ee228f2460e5fdfd0d22afefbe561e2390158
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-06-30 15:30:23 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,12 @@
 # This should generate a list of domains 1-20 wrapped around a root "word"
 
 ## variables
-DOCTOSPITOUT=/home/pi/anudeeptest/domainlist.txt
-ROOTSUBSLIST=/home/pi/anudeeptest/rootsubs.txt
+REPONAME=anudeeptest
+REPODIR=/home/pi/"$REPONAME"/
+REPOOWNER=deathbybandaid
+GITREPOSITORYURL="github.com/"REPOOWNER"/"REPONAME".git"
+DOCTOSPITOUT="$REPODIR"domainlist.txt
+ROOTSUBSLIST="$REPODIR"rootsubs.txt
 
 ## whiptail required
 WHATITIS=whiptail
@@ -11,14 +15,15 @@ WHATPACKAGE=whiptail
 if
 which $WHATITIS >/dev/null;
 then
-:
+echo "$WHATITIS Already Installed"
 else
 echo "Installing $WHATITIS"
 apt-get install -y $WHATPACKAGE
 fi
 
-## Touch file
-sudo touch $DOCTOSPITOUT
+## Remove old list
+rm $DOCTOSPITOUT
+touch $DOCTOSPITOUT
 
 for source in `cat $ROOTSUBSLIST`;
 do
@@ -32,3 +37,18 @@ echo "r"$i"---sn-"$source".googlevideo.com"
 done
 done
 echo "Script complete"
+
+Pushlists?
+{ if
+(whiptail --title "anudeeptest" --yesno "Do you want to push list to github" 10 80) 
+then
+GITHUBUSERNAME=$(whiptail --inputbox "Github Username?" 10 80 "" 3>&1 1>&2 2>&3)
+GITHUBPASSWORD=$(whiptail --inputbox "Github Password?" 10 80 "" 3>&1 1>&2 2>&3)
+GITWHERETOPUSH=https://"$GITHUBUSERNAME":"$GITHUBPASSWORD"@"$GITREPOSITORYURL"
+git -C $REPODIR remote set-url origin $GITWHERETOPUSH
+git -C $REPODIR add .
+git -C $REPODIR commit -m "Update lists $timestamp"
+git -C $REPODIR push -u origin master
+else
+:
+fi
