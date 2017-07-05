771a85b9ad8abe4efd0b3584a229651488f125b8
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-05 12:03:22 -0400

    Create runthis.sh

---
+++ script/runthis.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+## This will make it so that future runs go even smoother
+
+echo ""
+echo "____________________________________________________________________________"
+echo ""
+
+## variables
+source /etc/youtubeadsblacklist/script/vars/scriptvars.var
+
+## Pull down
+git -C $REPODIR pull
+
+echo "____________________________________________________________________________"
+echo ""
