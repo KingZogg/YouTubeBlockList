3271885f937030c6850a43cc597e812e32a7533f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-05 12:24:49 -0400

    Update pushlists.sh

--- script/pushlists.sh
+++ script/pushlists.sh
@@ -10,10 +10,11 @@ then
 timestamp=$(echo `date`)
 GITHUBUSERNAME=$(whiptail --inputbox "Github Username?" 10 80 "" 3>&1 1>&2 2>&3)
 GITHUBPASSWORD=$(whiptail --inputbox "Github Password?" 10 80 "" 3>&1 1>&2 2>&3)
+WHYYOUDODIS=$(whiptail --inputbox "Why are you doing a manual push?" 10 80 "Update lists $timestamp" 3>&1 1>&2 2>&3)
 GITWHERETOPUSH=https://"$GITHUBUSERNAME":"$GITHUBPASSWORD"@"$GITREPOSITORYURL"
 git -C $REPODIR remote set-url origin $GITWHERETOPUSH
 git -C $REPODIR add .
-git -C $REPODIR commit -m "Update lists $timestamp"
+git -C $REPODIR commit -m "$WHYYOUDODIS"
 git -C $REPODIR push -u origin master
 else
 :
