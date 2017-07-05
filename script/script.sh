2c6f570723cbb2a2a935bcf274a323ee1e1987f3
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-05 12:14:24 -0400

    Update script.sh

--- script/script.sh
+++ script/script.sh
@@ -129,19 +129,3 @@ mv $TEMPFILE $DOCTOSPITOUT
 
 HOWMANYLINES=$(echo -e "`wc -l $DOCTOSPITOUT | cut -d " " -f 1`")
 echo "New List Contains $HOWMANYLINES Domains."
-
-## Pushlists?
-if
-(whiptail --title "anudeeptest" --yesno "Do you want to push list to github" 10 80) 
-then
-timestamp=$(echo `date`)
-GITHUBUSERNAME=$(whiptail --inputbox "Github Username?" 10 80 "" 3>&1 1>&2 2>&3)
-GITHUBPASSWORD=$(whiptail --inputbox "Github Password?" 10 80 "" 3>&1 1>&2 2>&3)
-GITWHERETOPUSH=https://"$GITHUBUSERNAME":"$GITHUBPASSWORD"@"$GITREPOSITORYURL"
-git -C $REPODIR remote set-url origin $GITWHERETOPUSH
-git -C $REPODIR add .
-git -C $REPODIR commit -m "Update lists $timestamp"
-git -C $REPODIR push -u origin master
-else
-:
-fi
