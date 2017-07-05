eed79d18f533785b559fd794d5aa6f44c3d5a43f
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2017-07-05 12:31:21 -0400

    Update pushlists.sh

--- script/pushlists.sh
+++ script/pushlists.sh
@@ -12,6 +12,7 @@ GITHUBUSERNAME=$(whiptail --inputbox "Github Username?" 10 80 "" 3>&1 1>&2 2>&3)
 GITHUBPASSWORD=$(whiptail --inputbox "Github Password?" 10 80 "" 3>&1 1>&2 2>&3)
 WHYYOUDODIS=$(whiptail --inputbox "Why are you doing a manual push?" 10 80 "Update lists $timestamp" 3>&1 1>&2 2>&3)
 GITWHERETOPUSH=https://"$GITHUBUSERNAME":"$GITHUBPASSWORD"@"$GITREPOSITORYURL"
+sed "s/LASTRUNTIMEGOESHERE/$timestamp/" $MAINREADMEDEFAULT > $MAINREADME
 git -C $REPODIR remote set-url origin $GITWHERETOPUSH
 git -C $REPODIR add .
 git -C $REPODIR commit -m "$WHYYOUDODIS"
